ff2bc193563a3d59ec89c1dbd90bcb1ea31cdd29
Ashkan Abbasi <ashkan.abbasi66@yahoo.com>
2018-06-26 13:20:40 +0430

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -1,2 +1,8 @@
 a=np.array([[1,2,3],[4,5,6]])
 b=a[np.c_[0:2],np.r_[0:2]]
+
+# stack/concatenate along a new dimension.
+a=np.arange(0,6).reshape((2,3))
+b=np.arange(0,6).reshape((2,3))+10
+d=np.stack((a,b),axis=2)
+print(d[:,:,0])
