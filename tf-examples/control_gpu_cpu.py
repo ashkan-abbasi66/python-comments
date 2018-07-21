2466315e3086348707c8a35dbe9d64dfed51aebb
Ashkan Abbasi <ashkan.abbasi66@yahoo.com>
2018-07-21 20:39:25 +0430

    Update control_gpu_cpu.py

--- tf-examples/control_gpu_cpu.py
+++ tf-examples/control_gpu_cpu.py
@@ -1,7 +1,16 @@
+# GPU ALLOCATION
+# Assume that you have 12GB of GPU memory and want to allocate ~4GB:
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.45)
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+# sess=tf.Session()
+sess = tf.Session(config=session_conf)
+
+# determine number of CPU cores 
 # NUM_CORES = 1
 # inter_op_parallelism_threads=NUM_CORES, # add these lines to CongigProto
 # intra_op_parallelism_threads=NUM_CORES
 
+# It seems that it is different for Linux and windows.
 # Using GPU or CPU
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
@@ -12,13 +21,10 @@ session_conf = tf.ConfigProto(
       )
 
 
-# GPU ALLOCATION
-# Assume that you have 12GB of GPU memory and want to allocate ~4GB:
-# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.45)
-# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-# sess=tf.Session()
-sess = tf.Session(config=session_conf)
 
-# Don't use GPU!
-session_conf=tf.ConfigProto(device_count={'GPU': 0})
+
+# Don't use GPU! --- Windows
+os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+config = tf.ConfigProto(device_count = {'CPU': 1})
 sess=tf.Session(config=session_conf)
+
